5b98e46d9b81992e8833a2b573c7feee8479e3f0
Stanislav Ivanov <ivanov13sv@gmail.com>
2021-11-17 19:53:39 +0300

    debug routing

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -5,6 +5,7 @@ import {
   } from "react-router-dom";
 import About from './pages/About';
 import Contact from './pages/Contact';
+
 import Home from './pages/Home';
 import MySkills from './pages/MySkills';
 import Works from './pages/Works';
@@ -19,6 +20,7 @@ const AppRouter = () => {
             <Route path='/about' element={<About />} />
             <Route path='/skills' element={<MySkills />} />
             <Route path='/works' element={<Works />} />
+            <Route path='/contact' element={<Contact />} />
             <Route path="*" element={<Home /> }/>
         </Routes>
     );
